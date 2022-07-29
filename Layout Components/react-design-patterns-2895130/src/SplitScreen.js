2774796def0a809894a707911db120f000fa531f
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-07-29 16:27:16 +0530

    Split Screen width control using flex (var weight)
    Control the width of screens using flex

--- Layout Components/react-design-patterns-2895130/src/SplitScreen.js
+++ Layout Components/react-design-patterns-2895130/src/SplitScreen.js
@@ -5,17 +5,17 @@ const Container = styled.div`
   display: flex;
 `;
 const Pane = styled.div`
-  flex: 1;
+  flex: ${(props) => props.weight};
 `;
 
 // This is the way of renaming destructured variables in ES6. We changed name of components from left to Left and right to Right because it is React's convention of giving components capital letter names.
-export const SplitScreen = ({ left: Left, right: Right }) => {
+export const SplitScreen = ({ left: Left, right: Right, leftWeight = 1, rightWeight = 1 }) => {
   return (
     <Container>
-      <Pane>
+      <Pane weight={leftWeight}>
         <Left />
       </Pane>
-      <Pane>
+      <Pane weight={rightWeight}>
         <Right />
       </Pane>
     </Container>
