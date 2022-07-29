250fbb81bcb95ac0d05cac9e1c3371af833c022c
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-07-29 17:02:03 +0530

    Passing Components as children to Layout Component
    Instead of passing components as props we have passed components as children(elements) of the Layout Component

--- Layout Components/react-design-patterns-2895130/src/App.js
+++ Layout Components/react-design-patterns-2895130/src/App.js
@@ -1,15 +1,23 @@
 import { SplitScreen } from './SplitScreen';
 
-const LeftHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'green' }}>Left!</h1>;
+const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>;
 };
 
-const RightHandComponent = () => {
-  return <p style={{ backgroundColor: 'red' }}>Right!</p>;
+const RightHandComponent = ({ message }) => {
+  return <p style={{ backgroundColor: 'red' }}>{message}</p>;
 };
 
 function App() {
-  return <SplitScreen left={LeftHandComponent} right={RightHandComponent} leftWeight={1} rightWeight={3} />;
+  // return <SplitScreen left={LeftHandComponent} right={RightHandComponent} leftWeight={1} rightWeight={3} />;
+  // Instead of passing left and right components as props in SplitScreen component as above. layout components accept their children as children in the sense of react as below.
+  // This also helps in accepting props in a child component like below. If we would have passed left and right components as props in SplitScreen Component and not children then we would have to pass these names and message in left and right components through our SplitScreen Component then to left and right component in SplitScreen.js.
+  return (
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name={'Chinmaya'} />
+      <RightHandComponent message={'Hello World!'} />
+    </SplitScreen>
+  );
 }
 
 export default App;
--- Layout Components/react-design-patterns-2895130/src/SplitScreen.js
+++ Layout Components/react-design-patterns-2895130/src/SplitScreen.js
@@ -9,15 +9,28 @@ const Pane = styled.div`
 `;
 
 // This is the way of renaming destructured variables in ES6. We changed name of components from left to Left and right to Right because it is React's convention of giving components capital letter names.
-export const SplitScreen = ({ left: Left, right: Right, leftWeight = 1, rightWeight = 1 }) => {
+// export const SplitScreen = ({ left: Left, right: Right, leftWeight = 1, rightWeight = 1 }) => {
+//   return (
+//     <Container>
+//       <Pane weight={leftWeight}>
+//         <Left />
+//       </Pane>
+//       <Pane weight={rightWeight}>
+//         <Right />
+//       </Pane>
+//     </Container>
+//   );
+// };
+
+// Since we have not passed left and right components as props in SplitScreen component from App.js, instead passed the left and right components as children.
+// Therefore, We replaced left and right props with the children prop. Since, children prop is going to be an array in this case containg all of the elements that we paased as children to our SplitScreen component in App.js. We take out elements from children array as const [left, right] = children;
+// We have used left and right in small case and not in capitalize case because these are elements and not components.
+export const SplitScreen = ({ children, leftWeight = 1, rightWeight = 1 }) => {
+  const [left, right] = children;
   return (
     <Container>
-      <Pane weight={leftWeight}>
-        <Left />
-      </Pane>
-      <Pane weight={rightWeight}>
-        <Right />
-      </Pane>
+      <Pane weight={leftWeight}>{left}</Pane>
+      <Pane weight={rightWeight}>{right}</Pane>
     </Container>
   );
 };
