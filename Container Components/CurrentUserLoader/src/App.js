0781517f11d132fd0ce1cbeb9754542ef00414c2
ChinmayaGarg <chinmaya.garg1997@gmail.com>
2022-07-31 17:52:41 +0530

    Passed Props to child component without passing

--- Container Components/CurrentUserLoader/src/App.js
+++ Container Components/CurrentUserLoader/src/App.js
@@ -1,7 +1,14 @@
+import { CurrentUserLoader } from './CurrentUserLoader';
+import { UserInfo } from './UserInfo';
+
+// Here, even though there is no props that we can see, our CurrentUserLoader component is going to pass a user prop to our userInfo component,
+// just because UserInfo is one of its children.
 function App() {
-	return (
-		<h1>Your code goes here</h1>
-	);
+  return (
+    <CurrentUserLoader>
+      <UserInfo />
+    </CurrentUserLoader>
+  );
 }
 
 export default App;
